21e4fb096822d0e1974a222c75df76d1f8673dda
Simon Mulser <simonmulser@gmail.com>
2014-11-14 10:54:48 +0100

    add classpath and project file

--- src/main/java/vpmsbcm/App.java
+++ src/main/java/vpmsbcm/App.java
@@ -1,4 +1,4 @@
-package vpmsbcm;
+package main.java.vpmsbcm;
 
 /**
  * Hello world!
--- src/test/java/vpmsbcm/AppTest.java
+++ src/test/java/vpmsbcm/AppTest.java
@@ -1,4 +1,4 @@
-package vpmsbcm;
+package test.java.vpmsbcm;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
