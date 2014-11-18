e2fec4ef818a5607ff5eba18e5f61e58dc1b24a2
Simon Mulser <simonmulser@gmail.com>
2014-11-18 11:16:28 +0100

    rewrite warehouse to incoming listener

--- main/src/main/java/com/vpmsbcm/service/Incoming.java
+++ main/src/main/java/com/vpmsbcm/service/Incoming.java
@@ -4,29 +4,30 @@ import java.util.HashMap;
 
 import javax.annotation.PostConstruct;
 
+import org.openspaces.events.adapter.SpaceDataEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.vpmsbcm.common.model.CaseWithDetonator;
-import com.vpmsbcm.common.model.Good;
 import com.vpmsbcm.common.model.Load;
 import com.vpmsbcm.common.model.PropellingCharge;
+import com.vpmsbcm.common.model.Rocket;
 import com.vpmsbcm.common.model.Woodstick;
 import com.vpmsbcm.gui.WarehousePanel;
 
 @Component
-public class Warehouse {
+public class Incoming {
 
 	@Autowired
 	private WarehousePanel warehousePanel;
 
-	final Logger log = LoggerFactory.getLogger(Warehouse.class);
+	final Logger log = LoggerFactory.getLogger(Incoming.class);
 
 	private HashMap<Integer, PropellingCharge> cases;
 
-	public Warehouse() {
+	public Incoming() {
 	}
 
 	@PostConstruct
@@ -34,20 +35,33 @@ public class Warehouse {
 		cases = new HashMap<Integer, PropellingCharge>();
 	}
 
-	public synchronized void arrived(Good good) {
-		if (good instanceof Woodstick) {
-			Woodstick woodstick = (Woodstick) good;
-			warehousePanel.updateWoodstick(1);
-		} else if (good instanceof CaseWithDetonator) {
-			CaseWithDetonator caseWithDetonator = (CaseWithDetonator) good;
-			warehousePanel.updateCaseAndDetonator(1);
-		} else if (good instanceof PropellingCharge) {
-			PropellingCharge propellingCharge = (PropellingCharge) good;
-			warehousePanel.updatePropellingCharge(1);
-			cases.put(propellingCharge.getId(), propellingCharge);
-		} else if (good instanceof Load) {
-			Load load = (Load) good;
-			warehousePanel.updateLoad(1);
-		}
+	@SpaceDataEvent
+	public synchronized Woodstick eventListener(Woodstick event) {
+		warehousePanel.updateWoodstick(1);
+		return event;
+	}
+
+	@SpaceDataEvent
+	public synchronized CaseWithDetonator eventListener(CaseWithDetonator event) {
+		warehousePanel.updateCaseAndDetonator(1);
+		return event;
+	}
+
+	@SpaceDataEvent
+	public synchronized Load eventListener(Load event) {
+		warehousePanel.updateLoad(1);
+		return event;
+	}
+
+	@SpaceDataEvent
+	public synchronized PropellingCharge eventListener(PropellingCharge event) {
+		warehousePanel.updatePropellingCharge(1);
+		return event;
+	}
+
+	@SpaceDataEvent
+	public synchronized Rocket eventListener(Rocket event) {
+		// warehousePanel.updatePropellingCharge(1);
+		return event;
 	}
 }
