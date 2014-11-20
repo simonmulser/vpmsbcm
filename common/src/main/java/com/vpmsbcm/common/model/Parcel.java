e555bc058a5ba22fa682ebdfd5e599d3cef70106
Simon Mulser <simonmulser@gmail.com>
2014-11-20 16:35:23 +0100

    add member variables to parcel

--- common/src/main/java/com/vpmsbcm/common/model/Parcel.java
+++ common/src/main/java/com/vpmsbcm/common/model/Parcel.java
@@ -1,6 +1,41 @@
 package com.vpmsbcm.common.model;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.gigaspaces.annotation.pojo.SpaceId;
 
 public class Parcel {
+
+	private List<Rocket> rockets;
+
+	private static AtomicInteger ID = new AtomicInteger(0);
+
+	private Integer id;
+
+	public Parcel() {
+	}
+
+	public Parcel(List<Rocket> rockets) {
+		this.rockets = rockets;
+		this.id = ID.incrementAndGet();
+	}
+
+	@SpaceId(autoGenerate = false)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public List<Rocket> getRockets() {
+		return rockets;
+	}
+
+	public void setRockets(List<Rocket> rockets) {
+		this.rockets = rockets;
+	}
 
 }
