ecc10d7585b78b425d57fa65db8e80b33cc46250
Simon Mulser <simonmulser@gmail.com>
2015-01-05 16:28:37 +0100

    first impl of order service

--- order/src/main/java/com/vpmsbcm/service/Service.java
+++ order/src/main/java/com/vpmsbcm/service/Service.java
@@ -1,5 +1,7 @@
 package com.vpmsbcm.service;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.annotation.PostConstruct;
 
 import org.openspaces.core.GigaSpace;
@@ -18,9 +20,11 @@ public class Service {
 	final Logger log = LoggerFactory.getLogger(Service.class);
 
 	private int id;
+	private AtomicInteger orderId;
 
-	@GigaSpaceContext(name = "warehouseSpace")
-	private GigaSpace warehouseSpace;
+	@GigaSpaceContext(name = "warehouse")
+	private GigaSpace warehouse;
+
 	private GigaSpace space;
 
 	public Service() {
@@ -29,27 +33,31 @@ public class Service {
 	@PostConstruct
 	public void init() {
 		setId();
-
 		createSpace();
+		orderId = new AtomicInteger(0);
+
+		log.info("started service with id=" + id);
 	}
 
 	private void setId() {
 		IDFactory factory = new IDFactory();
 		factory.setId(1);
 
-		factory = warehouseSpace.take(factory, 1000);
+		factory = warehouse.take(factory, 1000);
+		System.out.println("!!!!" + warehouse);
 		if (factory != null) {
-			id = factory.getIdExporter();
-			factory.setIdExporter(id + 1);
-			warehouseSpace.write(factory);
-			log.info("started exporter with id=" + id);
+			id = factory.getIdProducer();
+			factory.setIdProducer(id + 1);
+			warehouse.write(factory);
+			log.info("started producer with id=" + id);
 		} else {
 			id = -1;
 		}
+
 	}
 
 	private void createSpace() {
-		space = new GigaSpaceConfigurer(new UrlSpaceConfigurer("/./order" + id)).gigaSpace();
+		space = new GigaSpaceConfigurer(new UrlSpaceConfigurer("/./order")).gigaSpace();
 	}
 
 	public GigaSpace getSpace() {
@@ -60,4 +68,7 @@ public class Service {
 		this.space = space;
 	}
 
+	public String getNextID() {
+		return "Order" + id + "Id" + orderId.incrementAndGet();
+	}
 }
