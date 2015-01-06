829d9d7595a6bf09319fc6f9f7f737990ecd5209
Simon Mulser <simonmulser@gmail.com>
2015-01-06 12:11:50 +0100

    merge of goods incoming into one class

--- main/src/main/java/com/vpmsbcm/service/listener/incoming/GoodIncoming.java
+++ main/src/main/java/com/vpmsbcm/service/listener/incoming/GoodIncoming.java
@@ -9,26 +9,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vpmsbcm.common.model.Charge;
+import com.vpmsbcm.common.model.Detonator;
+import com.vpmsbcm.common.model.Good;
 import com.vpmsbcm.common.model.Load;
+import com.vpmsbcm.common.model.Wood;
 import com.vpmsbcm.service.Warehouse;
 
 @EventDriven
 @Notify(gigaSpace = "warehouseSpace")
 @NotifyType(write = true)
-public class LoadIncoming {
+public class GoodIncoming {
 
 	@Autowired
 	private Warehouse warehouse;
 
-	final Logger log = LoggerFactory.getLogger(LoadIncoming.class);
+	final Logger log = LoggerFactory.getLogger(GoodIncoming.class);
 
-	public LoadIncoming() {
+	public GoodIncoming() {
 	}
 
 	@EventTemplate
-	Load unprocessedData() {
-		Load template = new Load();
+	Good unprocessedData() {
+		Good template = new Good();
 		return template;
+	}
+
+	@SpaceDataEvent
+	public Charge eventListener(Charge event) {
+		warehouse.newPropellingCharge(event);
+		return null;
+	}
+
+	@SpaceDataEvent
+	public Detonator eventListener(Detonator event) {
+		warehouse.updateCaseAndDetonator(1);
+		return null;
+	}
+
+	@SpaceDataEvent
+	public Wood eventListener(Wood event) {
+		warehouse.updateWoodstick(1);
+		return null;
 	}
 
 	@SpaceDataEvent
